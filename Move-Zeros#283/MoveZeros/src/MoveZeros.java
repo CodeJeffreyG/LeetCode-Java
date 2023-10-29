87dec222de743d28385aa82de06fedc36528ba8c
Jeffrey <jeffrey.graham600@gmail.com>
2023-10-29 00:29:03 -0400

    did more stuff

--- Move-Zeros#283/MoveZeros/src/MoveZeros.java
+++ Move-Zeros#283/MoveZeros/src/MoveZeros.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MoveZeros {
+public class MoveZeros {
+	public static void main(String[] args) {
+		int[] array = { 0, 1, 0, 3, 12 };
+		moveZeroes(array);
+
+	}
+
+	public static int[] moveZeroes(int[] nums) {
+		int i = 0;
+		int j = 1;
+
+		while (j < nums.length) {
+			if (nums[i] == 0 && nums[j] != 0) {
+				int saveJ = nums[j];
+				i = saveJ;
+				j = 0;
+			} else if (nums[i] == 0 && nums[j] == 0) {
+				j += 1;
+				continue;
+			}
+			j += 1;
+			i = j - 1;
+		}
+
+		return nums;
+
+	}
 }
