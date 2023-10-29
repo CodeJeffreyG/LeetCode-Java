5e2c59e61a042a6b7f62f50b503c8acef0babe4f
Jeffrey <jeffrey.graham600@gmail.com>
2023-10-29 10:51:32 -0400

    solved two sum two

--- Two_Sum_Two#167/TwoSumTwo/src/TwoSumTwo.java
+++ Two_Sum_Two#167/TwoSumTwo/src/TwoSumTwo.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class TwoSumTwo {
+public class TwoSumTwo {
+
+	public static void main(String[] args) {
+		int[] array = { 2, 7, 11, 15 };
+		twoSum(array, 9);
+	}
+
+	public static int[] twoSum(int[] numbers, int target) {
+		int i = 0;
+		int j = numbers.length - 1;
+		int[] answer = new int[0];
+
+		while (i < j) {
+			int complement = numbers[i] + numbers[j];
+
+			if (complement > target) {
+				j -= 1;
+			}
+
+			if (complement < target) {
+				i += 1;
+			}
+
+			if (complement == target) {
+				answer = new int[] { i + 1, j + 1 };
+				for (int num : answer) {
+					System.out.println(num);
+				}
+				return answer;
+			}
+		}
+
+		for (int num : answer) {
+			System.out.println(num);
+		}
+
+		return answer;
+	}
 }
