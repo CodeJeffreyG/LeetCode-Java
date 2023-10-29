83c507e4a0b8ed4bbb16efd91417bdc3069a56c8
Jeffrey <jeffrey.graham600@gmail.com>
2023-10-29 10:29:25 -0400

    solved moveZeros

--- Move-Zeros#283/MoveZeros/src/MoveZeros.java
+++ Move-Zeros#283/MoveZeros/src/MoveZeros.java
@@ -6,23 +6,17 @@ public class MoveZeros {
 	}
 
 	public static int[] moveZeroes(int[] nums) {
-		int i = 0;
-		int j = 1;
+		int start = 0;
 
-		while (j < nums.length) {
-			if (nums[i] == 0 && nums[j] != 0) {
-				int saveJ = nums[j];
-				i = saveJ;
-				j = 0;
-			} else if (nums[i] == 0 && nums[j] == 0) {
-				j += 1;
-				continue;
+		for (int i = 0; i < nums.length; i += 1) {
+
+			if (nums[i] != 0) {
+				int saveNumStart = nums[start];
+				nums[start] = nums[i];
+				nums[i] = saveNumStart;
+				start += 1;
 			}
-			j += 1;
-			i = j - 1;
 		}
-
 		return nums;
-
 	}
 }
